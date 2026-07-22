4b3a7ace49229b1bf926bab9cfb391baf2f6b182
Jyotiraditya Panda <jyotiraditya@aospa.co>
2026-07-22 18:51:42 +0000

    -- feat(theme): switch to pure AMOLED black with pink-red accent

--- app/src/main/java/dev/jyotiraditya/dmt/ui/theme/Color.kt
+++ app/src/main/java/dev/jyotiraditya/dmt/ui/theme/Color.kt
@@ -2,13 +2,13 @@ package dev.jyotiraditya.dmt.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val TuiBg = Color(0xFF0A0A0B)
+val TuiBg = Color(0xFF000000)
 val TuiSurface = Color(0xFF131415)
 val TuiFg = Color(0xFFC6CBCC)
 val TuiBright = Color(0xFFDFE4E5)
 val TuiDim = Color(0xFF767D80)
 val TuiFaint = Color(0xFF3C4245)
 val TuiLine = Color(0xFF1E2122)
-val TuiAccent = Color(0xFFE8A33D)
+val TuiAccent = Color(0xFFC2445C)
 val TuiRed = Color(0xFFB85C50)
 val TuiGreen = Color(0xFF7FA05F)
